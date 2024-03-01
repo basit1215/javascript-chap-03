519d40a1329ce7c746f11d0b01eb9f110436054f
basit1215 <haizabdulbasit225@gmail.com>
2024-03-01 22:49:12 +0500

    updated

---
+++ app.js
@@ -0,0 +1,18 @@
+ var age;
+ age=15;
+ alert("I am "+age+" years old");
+
+ var visit=0;
+visit=visit=14;
+alert( "You have visited this site "+visit+" times");
+
+var birthYear=2006 ;
+var visitorName="<b>Abdul Basit</b>";
+var productTitle="<b> T-shirt </b> (s)";
+var quantity="<b> 5 </b>";
+ document.write("My birth year is " +  birthYear + "</br>" + "Data type of my declared variable is number"  + "</br>" +   visitorName + " ordered " + quantity +  productTitle + " on XYZ Clothing store");  
+ 
+
+ 
+
+ 
